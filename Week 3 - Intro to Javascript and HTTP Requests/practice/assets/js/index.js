b3dcd446166ef1ded78eaf262695532ea60fd90b
Julia Sun <julia.sun@berkeley.edu>
2016-10-02 20:28:15 -0700

    Jquery

--- Week 3 - Intro to Javascript and HTTP Requests/practice/assets/js/index.js
+++ Week 3 - Intro to Javascript and HTTP Requests/practice/assets/js/index.js
@@ -1,14 +1,4 @@
-function bootstrapApp() {
-  // Add code here
-  console.log('DOM has loaded');
-}
-
-// Listen for changes to the document.readyState
-// `onreadystatechange` is fired when readyState value is changed
-document.onreadystatechange = function() {
-  // Check if readyState is interactive: DOM has loaded
-  if (document.readyState === 'interactive') {
-    bootstrapApp();
-  }
-}
+$(document).ready(function() {
+  // YOUR CODE GOES HERE
+});
 
