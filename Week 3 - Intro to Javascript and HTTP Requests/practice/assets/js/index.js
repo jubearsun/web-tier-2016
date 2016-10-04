0a1b9e51ad4177edbd7bb2118ac43f92fe4f3d72
Julia Sun <julia.sun@berkeley.edu>
2016-10-03 20:44:03 -0700

    request example

--- Week 3 - Intro to Javascript and HTTP Requests/practice/assets/js/index.js
+++ Week 3 - Intro to Javascript and HTTP Requests/practice/assets/js/index.js
@@ -1,4 +1,61 @@
 $(document).ready(function() {
-  // YOUR CODE GOES HERE
+
+  $('#color__gen').click(function(e) {
+    superagent.get('http://webtier2016.christianle.com/v1/color')
+    .end(function(err, res) {
+      if (err) {
+        $('.hex__response')
+          .text("Your request failed...please try again.")
+          .css({
+            color: 'red'
+          });
+        console.log(err);
+      } else {
+        $('#color__gen')
+          .css({
+            border: '2px solid ' + res.body.hex,
+            color: res.body.hex
+          })
+        $('#hex__response')
+          .text(res.body.hex)
+          .css({color: res.body.hex});
+        console.log(res);
+      }
+    });
+  })
+
+  $('#submit').click(function(e) {
+    e.preventDefault();
+
+    var sender = $('#name').val();
+    var addr = $('#email').val();
+    var msg = $('#message').val();
+
+    superagent.post('http://webtier2016.christianle.com/v1/contact')
+      .send({
+        name: sender,
+        email: addr,
+        message: msg
+      })
+      .end(function(err, res) {
+        if (err) {
+          $('#message__response')
+            .text("Your request failed...please try again.")
+            .css({
+              color: 'red'
+            })
+          console.log("YOU FAILED");
+          console.log(err);
+        } else {
+          $('#message__response')
+            .text(res.body.message)
+            .css({
+              color: 'green'
+            })
+          console.log("YOU SUCCEEDED!!!! huehuehue");
+          console.log(res);
+        }
+      })
+  });
 });
 
